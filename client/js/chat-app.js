33e319d68a8d1c2ac2a09c1bc2402bfcf51cbb9a
TJ Liwanag <timothyjliwanag@gmail.com>
2016-11-02 14:35:04 -0400

    initial commit

---
+++ client/js/chat-app.js
@@ -0,0 +1,16 @@
+(function)() {
+        'use strict';
+
+        window.chat.listenForMessages(function messageHandler(data) {
+            var userName
+        });
+
+        console.log('ready?');
+    )};
+
+        
+
+
+
+
+})();
