f0a5b2b7731a8cfcbb03d07c806297bf755d3b94
TJ Liwanag <timothyjliwanag@gmail.com>
2016-11-02 16:06:48 -0400

    created jQuery pointing to ajax with url, data, and headers

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -1,14 +1,34 @@
-(function)() {
+(function () {
         'use strict';
 
-        window.chat.listenForMessages(function messageHandler(data) {
-            var userName
-        });
+        // console.log('ready');
 
-        console.log('ready?');
-    )};
+        window.chat.listenForMessages(function messageHandler(data) { });
+        //     var userName =
+        //     $.listenForMessages
 
-        
+        $.ajax({
+            url: '/login',
+            data:'JSON.stringify'({
+                    userName: $('.userName').val()
+            }),
+            headers: {
+                    'Content-Type': 'application/JSON',
+            }
+        })
+
+
+        $.login
+                .on('submit', function userName(event){
+
+        })
+
+        .done(function successHandler(data){
+                console.log('it works!', data);
+        })
+        .fail(function failHandler(xhr){
+                console.log('no soup', xhr);
+        })
 
 
 
