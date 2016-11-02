fbe77c62fbc1d9be5d9a15090a349fd67e3d0d78
TJ Liwanag <timothyjliwanag@gmail.com>
2016-11-02 17:36:19 -0400

    updated function for login

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -3,26 +3,61 @@
 
         // console.log('ready');
 
-        window.chat.listenForMessages(function messageHandler(data) { });
+        window.chat.listenForMessages(function messageHandler(data) {
+                console.log('message received', data);
+                // TODO: add the new message to the UI
+        });
         //     var userName =
         //     $.listenForMessages
 
-        $.ajax({
-            url: '/login',
-            data: JSON.stringify({
-                    userName: $('.userName').val()
-            }),
-            headers: {
-                    'Content-Type': 'application/json',
-            }
-        })
-
-
         $('.login')
-                .on('submit', function userName(event){
+                .on('submit', function username(event){
                         console.log("Clicked Submit", event);
                         event.preventDefault();
+                        $.ajax({
+                                url: '/login',
+                                method: 'POST',
+                                data: JSON.stringify({
+                                username: $('.username').val()
+                                }),
+                                headers: {
+                                    'Content-Type': 'application/json',
+                            }
+                        })
+                        .done(function finished(data){
+                                console.log('logged in!', data);
+                                // TODO: hold onto the token from inside the data object!
+                        })
                 })
+
+
+
+                // TODO: use the token and message from the form to send a message to the server
+                // once the message form is submitted
+
+
+        $('.chat')
+                .on('send-message', function messenger(event){
+                        console.log("", event)
+                        event.preventDefault();
+                        $.ajax({
+                                url: 'sendmessage',
+                                method: 'POST',
+                                data: JSON.stringify({
+                                message: $('.message').val()
+                                }),
+                                headers: {
+                                        Authorization: token,
+                                        'Content-Type': 'application/json',
+                                }
+                        })
+                })
+
+
+
+
+
+
                 // .done(function successHandler(data){
                 //         console.log('it works!', data);
                 // })
